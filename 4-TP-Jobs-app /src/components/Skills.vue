3627d6eb54bd3881369337df16b52dda712742dd
Youness Anouar <146961049+Uness10@users.noreply.github.com>
2024-04-30 20:41:02 +0100

    Add files via upload

---
+++ 4-TP-Jobs-app /src/components/Skills.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="skills">
+      <div class="skill" v-for="s in skills" :key="s">{{ s }}</div>
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    name: 'Skills',
+    props: {
+      skills: Array
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .skills {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  
+  .skill {
+    background-color: #f0f0f0;
+    color: #333;
+    padding: 5px 10px;
+    margin: 5px;
+    border-radius: 5px;
+  }
+  </style>
+  
